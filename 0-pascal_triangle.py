444eca2f713ed8a485e6766d406273c06751ea78
Constance Amatey <consamate57@gmail.com>
2024-03-12 20:03:04 +0000

    pascal

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""that returns a list of lists of integers representing the Pascals triangle"""
+
+def pascal_triangle(n):
+    """function that returns integers that draw a triangle"""
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        new_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                new_list.append(1)
+            else:
+                new_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(new_list)
+
+    return triangle
