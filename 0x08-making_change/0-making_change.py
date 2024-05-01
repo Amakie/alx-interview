1e58d36ca0c776c980eb1331c3bc64a9e419d404
Constance Amatey <consamate57@gmail.com>
2024-05-01 17:29:46 +0000

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,7 +12,9 @@ def makeChange(coins, total):
 
     for t in range(1, total + 1):
         for coin in coins:
-            if t - coin >= 0:
+            if coin > total:
+                pass
+            elif t - coin >= 0:
                 dp[t] = min(dp[t], 1 + dp[t - coin])
 
     return dp[total] if dp[total] != total + 1 else -1
