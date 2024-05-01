b27235b7951e3163c9633eb91dddb6fb87aa4b72
Constance Amatey <consamate57@gmail.com>
2024-05-01 17:34:02 +0000

    improving efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,7 +12,7 @@ def makeChange(coins, total):
 
     for t in range(1, total + 1):
         for coin in coins:
-            if coin > total:
+            if coin > t:
                 pass
             elif t - coin >= 0:
                 dp[t] = min(dp[t], 1 + dp[t - coin])
