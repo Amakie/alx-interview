931f7768ca4e75b3de1cb74fe3237a112598385b
Constance Amatey <consamate57@gmail.com>
2024-05-01 17:21:46 +0000

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,9 +2,9 @@
 """change making problem"""
 
 
-def makeChange(coins: int, total: int) -> int:
+def makeChange(coins, total):
     """function that returns the fewest number of coins"""
-    if total <= 0:
+    if total < 0:
         return 0
 
     dp = [total + 1] * (total + 1)
@@ -12,7 +12,7 @@ def makeChange(coins: int, total: int) -> int:
 
     for t in range(1, total + 1):
         for coin in coins:
-            if t - coins >= 0:
-                dp[t] = (min(dp[t]), 1 + dp[t - coin])
+            if t - coin >= 0:
+                dp[t] = min(dp[t], 1 + dp[t - coin])
 
     return dp[total] if dp[total] != total + 1 else -1
